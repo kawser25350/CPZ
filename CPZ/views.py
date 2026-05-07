973196cb470e3e5fde9073723aa6195ec1c89cee
Kawser Ahmmed <136078973+kawser25350@users.noreply.github.com>
2026-05-07 19:32:32 +0600

    phase-2 frontent

--- CPZ/views.py
+++ CPZ/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from posts.models import Post
 from category.models import Category
@@ -5,13 +6,24 @@ from category.models import Category
 def home(request):
     cat_data = Category.objects.all()
     selected_category = request.GET.get('category')
+    search_query = request.GET.get('q', '').strip()
     
     post_data = Post.objects.all()
     if selected_category:
         post_data = post_data.filter(category__id=selected_category).distinct()
+    if search_query:
+        post_data = post_data.filter(
+            Q(name__icontains=search_query)
+            | Q(content__icontains=search_query)
+            | Q(author__username__icontains=search_query)
+            | Q(author__first_name__icontains=search_query)
+            | Q(author__last_name__icontains=search_query)
+            | Q(category__name__icontains=search_query)
+        ).distinct()
     
     return render(request, 'pages/home.html', {
         'post_data': post_data,
         'cat_data': cat_data,
         'selected_category': selected_category,
+        'search_query': search_query,
     })
